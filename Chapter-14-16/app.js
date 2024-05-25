90005b55de38411f27ac12afe255e842d00b76f8
Alishba-Khan02 <ak6445268@gmail.com>
2024-05-25 14:15:54 +0500

    Added chapter 14-16

---
+++ Chapter-14-16/app.js
@@ -0,0 +1,88 @@
+//question -01
+// var students =[];
+
+//Question-02
+// var students = Array();
+
+//Question-03
+// var colors=["Red", "Yellow", "Blue", "Green"];
+
+//Question-04
+// var numbers =[10,208,300,450];
+
+//Question-05
+// var boolean=[true,false,false,true];
+
+//Question-06
+// var mixed=["ali",200,true,"Red"];
+
+//Question-07
+// var qualification =["SSC", "HSC", "BCS","BS","BCOM","MS","M.Phil", "phD"];
+// document.write("<h2> Qualifications </h2> <br>")
+// document.write(" <h4> 1) SSC </h4>");
+// document.write(" <h4> 2) HSC </h4>");
+// document.write(" <h4> 3) BCS </h4>");
+// document.write(" <h4> 4) BS </h4>");
+// document.write(" <h4> 5) BCOM </h4>");
+// document.write(" <h4> 6) MS </h4>");
+// document.write(" <h4> 7) M.Phil </h4>");
+// document.write(" <h4> 8) phD </h4>");
+
+//Question-08
+// var students=["Alishba", "Ayesha","Sana"];
+// var score=[400,350,200];
+// var total=550;
+// percentage1=(score[0]/total)*100;
+// percentage2=(score[1]/total)*100;
+// percentage3=(score[2]/total)*100;
+// document.write("Score of Alishba is 400.Percentage:72.7% <br>");
+// document.write("Score of Ayesha is 350.Percentage:63.6% <br>");
+// document.write("Score of Sana is 200.Percentage:36.6% <br>");
+
+// //Question-09
+// var colors =[];
+// //9a;
+// var userColor=prompt("Enter the color you want to add in the begining");
+// colors.unshift(userColor);
+// //9b;
+// var user_end_color=prompt("Enter the color you want to add in the end");
+// colors.push(user_end_color);
+// document.write('Updated array: '+user_end_color+' <br>');
+// //9c;
+// var color1=prompt("Enter first color for added in the beginning");
+// var color2=prompt("Enter second color for added in the beginning");
+// colors.unshift(color1,color2);
+// document.write('Updated array; '+colors+'<br>');
+// //9d;
+// colors.shift();
+// document.write('Updated array:'+colors+'<br>');
+// //9e;
+// colors.pop();
+// document.write('Updated array:'+colors+'<br>');
+// //9f;
+// var index=+prompt("At which index you want to add colour?");
+// var colorName=("also write color name");
+// colors.splice(index,colorName);
+// document.write('Updated array:'+colors+'<br>');
+// //9g;
+// var index =+prompt("At which index you want to delete color?");
+// var deleteColor=+prompt("How many colours you want to delete?");
+// colors.splice(index,deleteColors);
+// document.write('Updated array:'+colors+'<br>');
+
+//Question-10
+// var score=[200,120,130];
+// document.write('Score of students:'+score+'<br>');
+// document.write('Ordered score of students:'+score.sort()+'<br>');
+
+//Question-11
+// var citynames=['Karachi','Lahore','Multan','Isl','Quetta'];
+// document.write('Cities List :'+citynames+'<br>'+'<br>')
+// var selectedCities=citynames.slice(2,5);
+// document.write('Selectedcities List :'+selectedCities+'<br>')
+
+//Question-12
+// var array=['This','is','my','cat'];
+// document.write('Array : '+'<br>'+array+'<br>');
+// var string=array.join(' ');
+// document.write('String : '+'<br>'+string);
