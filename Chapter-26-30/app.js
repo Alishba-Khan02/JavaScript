c7197f096d00fd6d3a1f8d5ea33bcee498c777b6
Alishba-Khan02 <ak6445268@gmail.com>
2024-07-01 10:15:00 +0500

    added chap:26-30

---
+++ Chapter-26-30/app.js
@@ -0,0 +1,65 @@
+//Question-01
+var num = +prompt("Enter number: ");
+var round=Math.round(num);
+var floor=Math.floor(num);
+var ceil=Math.ceil(num);
+document.write('Number :'+num+'<br>')
+document.write('Round off value :'+round+'<br>')
+document.write('Floor value :'+floor+'<br>')
+document.write('ceil value :'+ceil+'<br>')
+
+//Question-02
+//  var num = +prompt("Enter number: ");
+// var round=Math.round(num);
+// var floor=Math.floor(num);
+// var ceil=Math.ceil(num);
+// document.write('Number :'+num+'<br>')
+// document.write('Round off value :'+round+'<br>')
+// document.write('Floor value :'+floor+'<br>')
+// document.write('ceil value :'+ceil+'<br>')
+
+//Question-03
+// var num=+prompt('Enter number :');
+// var abs=Math.abs(num);
+// document.write('The absolute value of '+num+'  is '+abs)
+
+//Question-04
+// var randomNo=Math.random()*6;
+// var floor=Math.floor(randomNo);
+// document.write('random die value : '+floor)
+
+
+//Question-05
+// var coinValue=Math.random()*3;
+// var floor=Math.floor(coinValue);
+// console.log(floor)
+// if(coinValue<2){
+//     document.write(floor+'<br>'+' random coin value : Tails')
+// }
+// else{
+//     document.write(floor+'<br>'+' random coin value : Heads')
+// }
+
+//Question-06
+// var random=Math.random()*100;
+// var ceil=Math.ceil(random);
+// document.write('random number between 1 and 100:'+ceil)
+
+
+//Question-07
+// var weight=prompt('Enter your weight in kilogramas');
+// var parse=parseFloat(weight);
+// document.write('The weight of userr is '+parse.toFixed(1)+' kilograms')
+
+//Question-08
+// var randomNo=Math.ceil((Math.random()*10));
+// document.write(randomNo)
+// var userInput=prompt('Enter a number in between 1 and 10');
+// if(userInput===randomNo){
+//     alert('Congratulations  you entered the same number')
+// }
+// else{
+//     alert('Try Again')
+// }
+
+
