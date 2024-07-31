2592867c53b059bdb7899d1d7e0d965806e5f61c
Alishba-Khan02 <ak6445268@gmail.com>
2024-07-31 17:22:54 +0500

    Adding chap:43-48

---
+++ Chapter-43-48/app.js
@@ -0,0 +1,36 @@
+
+//  Question-01
+
+// function greet(){
+//     alert('Hello User')
+// }
+
+//   Question-02
+
+function thanks(){
+    alert('Thanks purchasing phone from us')
+}
+
+//   Question-03
+
+// function remove(e){
+//     e.parentNode.parentNode.remove()
+
+// }
+
+//  Question-04
+
+// on index.html
+
+// Question-05
+
+var counter=0
+function increase(){
+    document.getElementById("counter").innerText=counter++
+}
+function decrease(){
+    if (counter<0){
+        counter=0
+    }
+    document.getElementById("counter").innerText=counter--
+}
